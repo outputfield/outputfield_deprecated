3049cf361428f6d671b2aa76ba3e73ac903442db
Andrew Huang <andrw.huang@gmail.com>
2022-09-02 16:59:08 -0700

    stringify file for upload

--- pages/api/uploadFile.ts
+++ pages/api/uploadFile.ts
@@ -1,15 +1,16 @@
-import { ListObjectsCommand, PutObjectCommand } from '@aws-sdk/client-s3'
+import { PutObjectCommand } from '@aws-sdk/client-s3'
 import { NextApiRequest, NextApiResponse } from 'next'
 import spaces from '../../lib/doSpaces'
 
 export default async function uploadFile(req: NextApiRequest, res: NextApiResponse) {
   const { file } = req.body
   try {
+    // TODO: Specify type here
     // Specifies a path within your Space and the file to upload.
     const bucketParams = {
       Bucket: 'outputfieldartworks',
       Key: file.name,
-      Body: file,
+      Body: JSON.stringify(file),
       ACL: 'public-read'
     }
     const data = await spaces.send(new PutObjectCommand(bucketParams))
