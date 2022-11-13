e316f8bc0eed812337c8c337251f9ac1ee12e1d4
Andrew Huang <andrw.huang@gmail.com>
2022-11-13 14:10:34 -0800

    new prisma migration, package lock

--- prisma/migrations/20221113224753_/migration.sql
+++ prisma/migrations/20221113224753_/migration.sql
@@ -5,6 +5,7 @@ CREATE TABLE "User" (
     "password" TEXT,
     "email" VARCHAR(255) NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "nominatorId" INTEGER,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -21,7 +22,6 @@ CREATE TABLE "Artist" (
     "handle" VARCHAR(255) NOT NULL,
     "mediums" TEXT[],
     "mediumsOfInterest" TEXT[],
-    "referrerId" INTEGER,
     "userId" INTEGER NOT NULL,
 
     CONSTRAINT "Artist_pkey" PRIMARY KEY ("id")
@@ -40,15 +40,12 @@ CREATE TABLE "Work" (
 -- CreateTable
 CREATE TABLE "Link" (
     "id" SERIAL NOT NULL,
-    "type" TEXT DEFAULT E'',
-    "link" TEXT NOT NULL DEFAULT E'',
+    "label" TEXT DEFAULT E'',
+    "url" TEXT NOT NULL DEFAULT E'',
     "artistID" INTEGER NOT NULL,
 
     CONSTRAINT "Link_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_name_key" ON "User"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
@@ -59,17 +56,14 @@ CREATE UNIQUE INDEX "Artist_handle_key" ON "Artist"("handle");
 -- CreateIndex
 CREATE UNIQUE INDEX "Artist_userId_key" ON "Artist"("userId");
 
--- CreateIndex
-CREATE UNIQUE INDEX "Artist_referrerId_key" ON "Artist"("referrerId");
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_nominatorId_fkey" FOREIGN KEY ("nominatorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Artist" ADD CONSTRAINT "Artist_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Artist" ADD CONSTRAINT "Artist_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Artist" ADD CONSTRAINT "Artist_referrerId_fkey" FOREIGN KEY ("referrerId") REFERENCES "Artist"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Work" ADD CONSTRAINT "Work_artistID_fkey" FOREIGN KEY ("artistID") REFERENCES "Artist"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Work" ADD CONSTRAINT "Work_artistID_fkey" FOREIGN KEY ("artistID") REFERENCES "Artist"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Link" ADD CONSTRAINT "Link_artistID_fkey" FOREIGN KEY ("artistID") REFERENCES "Artist"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Link" ADD CONSTRAINT "Link_artistID_fkey" FOREIGN KEY ("artistID") REFERENCES "Artist"("id") ON DELETE CASCADE ON UPDATE CASCADE;
