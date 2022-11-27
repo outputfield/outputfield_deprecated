5c8a3d35dc4c0a126df09114873437c08910cd56
Andrew Huang <andrw.huang@gmail.com>
2022-11-27 14:57:28 -0800

    WIP, switched to headless UI for modal and tabs. TODO styling for tabs

--- components/profileForm.tsx
+++ components/profileForm.tsx
@@ -3,8 +3,9 @@ import { useForm, useFieldArray, SubmitHandler } from 'react-hook-form'
 import Input from './formInput'
 import { Button } from './button/button.component'
 import DropzoneComponent from './dropzoneComponent'
-import Overlay from './overlay'
 import TabView from './tabView/tabView.component'
+import { Dialog } from '@headlessui/react'
+import Image from 'next/image'
 
 type ProfileLink = {
   url: string;
@@ -84,6 +85,51 @@ export default function ProfileForm({ onSubmit, isSubmitting, profile }: Props) 
 
   return (
     <>
+      <Dialog
+        open={uploadOpen}
+        onClose={() => setUploadOpen(false)}
+        className="relative z-50"
+      >
+        {/* The backdrop, rendered as a fixed sibling to the panel container */}
+        <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+
+        {/* Full-screen container to center the panel */}
+        <div className="fixed inset-0 flex items-center justify-center ">
+          {/* The actual dialog panel  */}
+          <Dialog.Panel className="mx-auto max-w-sm bg-white min-h-screen min-w-full">
+            {/* <Dialog.Title>Complete your order</Dialog.Title> */}
+
+            <div className="mt-0 sm:mt-0 sm:ml-4 flex justify-between border-b border-black border-dashed px-4 pb-4">
+              <button onClick={(e) => {
+                e.preventDefault()
+                setUploadOpen(false)
+              }}>
+                <Image src="/closeIcon.svg" alt="close" width={16} height={16} />
+              </button>
+            </div>
+            <div className="my-5 text-center">
+
+              <TabView headers={['Upload', 'Embed']}>
+                <div className='UploadPanel'>
+
+                   We currently support:
+                  <br /><br />
+                  <ul>
+                    <li>images (jpg, png, gif, tiff)</li>
+                  </ul>
+                  <br />
+                  <MemoDropzoneComponent uploadNum={uploadNum} />
+                </div>
+                <div className="EmbedPanel">
+                  {/* <Input placeholder="Link Youtuboe, Vime, SoundCloud, etc." label={`links.${uploadNum}.url`} {...register(`links.${uploadNum}.url`)} />
+          <Input placeholder="label" label={`links.${uploadNum}.label`} {...register(`links.${uploadNum}.label`)} /> */}
+                  <Button>Embed</Button>
+                </div>
+              </TabView>
+            </div>
+          </Dialog.Panel>
+        </div>
+      </Dialog>
       
       <form onSubmit={handleSubmit(
         ((data: any, e: BaseSyntheticEvent) => onSubmit(e, data, Object.values(state))) as SubmitHandler<ISignUpInputs>)} className="w-full max-w-lg">
@@ -193,37 +239,6 @@ export default function ProfileForm({ onSubmit, isSubmitting, profile }: Props) 
 
         <div className="flex flex-wrap -mx-3 mb-2">
           <h2>Upload Work</h2>
-          <Overlay className={uploadOpen ? 'visible' : 'hidden'}>
-            <div className="mt-0 sm:mt-0 sm:ml-4 flex justify-between border-b border-black border-dashed px-4 pb-4">
-              <button onClick={(e) => {
-                e.preventDefault()
-                setUploadOpen(false)
-              }}>
-                <img src="/closeIcon.svg" alt="close overlay icon" />
-              </button>
-            </div>
-            <div className="my-5 text-center">
-
-              <TabView headers={['Upload', 'Embed']}>
-                <div className='UploadPanel'>
-
-            We currently support:
-                  <br /><br />
-                  <ul>
-                    <li>images (jpg, png, gif, tiff)</li>
-                  </ul>
-                  <br />
-
-                  <MemoDropzoneComponent uploadNum={uploadNum} />
-                </div>
-                <div className="EmbedPanel">
-                  {/* <Input placeholder="Link Youtuboe, Vime, SoundCloud, etc." label={`links.${uploadNum}.url`} {...register(`links.${uploadNum}.url`)} />
-            <Input placeholder="label" label={`links.${uploadNum}.label`} {...register(`links.${uploadNum}.label`)} /> */}
-                  <Button>Embed</Button>
-                </div>
-              </TabView>
-            </div>
-          </Overlay>
           <div className="w-full md:w-1/3 px-4 py-6 mb-6 md:mb-0 border border-dashed border-black">
             <div className="py-6 grid grid-cols-2 justify-items-center">
               {[0,1,2,3,4,5].map((key) => {
