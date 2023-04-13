1d1813afeab9c8aeb0816b9899fe24de17bb5a2f
Michelle Akin <michelle.git8@gmail.com>
2023-04-12 21:07:07 -0700

    WIP seed file, remaining: seed artists

--- prisma/seed_v2.ts
+++ prisma/seed_v2.ts
@@ -22,29 +22,91 @@ const main = async () => {
       email: "lacelove@gmaiil.com",
     },
   });
+  console.log(
+    `successfully created admin user: ${adminUser.name}, id: ${adminUser.id}`
+  );
 
   const adminInvite = await prisma.invitation.create({
     data: {
       inviterId: {
-        connect: { id: adminUser.id }, // TODO: continue here
+        connect: { id: adminUser.id },
       },
     },
   });
+  console.log(`successfully created admin invitation: ${adminInvite.id}`);
+
+  const adminInviteId = adminInvite.id;
 
   // iterate over applications and create the test applications
+  applications.forEach(async (application) => {
+    console.log(`creating application: ${application}`);
+
+    const app = await prisma.application.create({
+      data: {
+        ...application,
+        invitationId: { connect: { id: adminInviteId } },
+      },
+    });
+
+    console.log(`successfully created application: ${application.name}`);
+
+    const user = await prisma.user.create({
+      data: {
+        name: application.name,
+        email: application.email,
+        applicationId: { connect: { id: app.id } },
+      },
+    });
+
+    const artist = await prisma.artist.create({
+      // TODO: add artist data
+    });
+  });
 };
 
-const testInvitationId = "4de8cff0-3f05-4755-b4c9-64753507fe48";
 const applications = [
   {
     name: "Jonny Riches",
-    statement: "jr@gmail.com",
     email: "jr@gmail.com",
-    requiredLink: "",
-    invitationId: "",
+    statement: "He is super funny and talented",
+    requiredLink: "johnnyriches.com",
+  },
+  {
+    name: "Ibrahim Hopkins",
+    email: "hopkins@email.com",
+    statement:
+      "He is an artist who documents jovial expression of life with sculptures of babies, who inspire him into incorporating himself at a young age.",
+    requiredLink: "ibrahimhopkins.com",
+  },
+  {
+    name: "Sofia Wheeler",
+    email: "wheelerS@email.com",
+    statement:
+      "When we have some moments in our lives, we want to show people how important those moments are to us in certain ways.",
+    requiredLink: "sofiawheeler.com",
+  },
+  {
+    name: "Connor Garrison",
+    email: "conngar@email.com",
+    statement:
+      "My career for a few years spent designing for the web, but I have become more passionate since then.",
+    requiredLink: "conngarrison.com",
+  },
+  {
+    name: "Selima Khalil",
+    email: "kelima@email.com",
+    statement:
+      "Selima Khalil is an artist and DIY designer working within the realm of contemporary embroidery.",
+    requiredLink: "selimakhalil.com",
+  },
+  {
+    name: "Lily Liu",
+    email: "llll@email.com",
+    statement:
+      "Lily Liu creates vector designs associating loss and diaspora to the presence of the external environment, an element of which is a loss vector which is always negative.",
+    requiredLink: "lilyliu.com",
   },
 ];
-const generateApplications = () => {};
 
 main()
   .then(async () => {
